6205cbd206d2ad14088c8184199482c5cca2276d
bohdanpc <bohdanpc@gmail.com>
2017-05-11 22:09:00 +0300

    tiny change for Tree supporting

--- Scanner/Scanner/parser.cpp
+++ Scanner/Scanner/parser.cpp
@@ -10,6 +10,16 @@ enum {
 	//...etc
 } nonterminalCode;
 
+#define LEN 50
+typedef struct tNode {
+	lex_string Token;
+	char nonTerminalName[LEN] = { 0 };
+	vector<tNode*> children;
+}Node;
+
+static Node *Tree = NULL;
+
+
 void initNonTerminals() {
 	int i = 1;
 	NonTerminals.insert(pair<uint, string>(i++, "<signal_program>"));
@@ -17,13 +27,6 @@ void initNonTerminals() {
 	NonTerminals.insert(pair<uint, string>(i++, "<procedure_identifier>"));
 	//... etc
 }
-
-#define LEN 50
-typedef struct tNode{
-	lex_string Token;
-	char nonTerminalName[LEN] = { 0 };
-	vector<tNode> children;
-}Node;
 
 
 lex_string get_next_token() {
@@ -38,49 +41,49 @@ lex_string get_next_token() {
 }
 
 
-void nonterminal_IDN(lex_string &curr_token) {
+void nonterminal_IDN(Node *&Tree, lex_string &curr_token) {
 	//
 	curr_token = get_next_token();
 }
 
-void nonterminal_PROC_IDN(lex_string &curr_token) {
-	nonterminal_IDN(curr_token);
+void nonterminal_PROC_IDN(Node *&Tree, lex_string &curr_token) {
+	nonterminal_IDN(Tree, curr_token);
 }
 
-void nonterminal_DECLARATIONS(lex_string &curr_token) {
-
-}
-
-void nonterminal_STATEMENT_LST(lex_string &curr_token) {
+void nonterminal_DECLARATIONS(Node *&Tree, lex_string &curr_token) {
 
 }
 
-void nonterminal_BLOCK(lex_string &curr_token) {
-	nonterminal_DECLARATIONS(curr_token);
+void nonterminal_STATEMENT_LST(Node *&Tree, lex_string &curr_token) {
+
+}
+
+void nonterminal_BLOCK(Node *&Tree, lex_string &curr_token) {
+	nonterminal_DECLARATIONS(Tree, curr_token);
 
 	if (curr_token.lex_code != search_key_tab("BEGIN"))
 		cerr << "'BEGIN' KEY_WORD EXPECTED\n";
 	else {
 		curr_token = get_next_token();
-		nonterminal_STATEMENT_LST(curr_token);
+		nonterminal_STATEMENT_LST(Tree, curr_token);
 
 		if (curr_token.lex_code != search_key_tab("END"))
 			cerr << "'END' KEY_WORD EXPECTED\n";
 	}
 }
 
-void nonterminal_PROGRAM(lex_string &curr_token) {
+void nonterminal_PROGRAM(Node *&Tree, lex_string &curr_token) {
 	if (curr_token.lex_code != search_key_tab("PROGRAM"))
 		cerr << "'PROGRAM' KEY_WORD expected...";
 	else {
 		curr_token = get_next_token();
-		nonterminal_PROC_IDN(curr_token);
+		nonterminal_PROC_IDN(Tree, curr_token);
 
 		if (curr_token.lex_code != (uint)';')
 			cerr << "';' EXPECTED\n";
 		else {
 			curr_token = get_next_token();
-			nonterminal_BLOCK(curr_token);
+			nonterminal_BLOCK(Tree, curr_token);
 		}
 
 		if (curr_token.lex_code != (uint)';')
